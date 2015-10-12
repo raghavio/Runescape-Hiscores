027bc34bfedc289b7802a4633d8d09da2850b3aa
Raghav Sharma <raghav_ftw@hotmail.com>
2015-10-12 22:40:40 +0530

    Improved and simplified model

--- runescape/hiscores/models.py
+++ runescape/hiscores/models.py
@@ -1,128 +1,108 @@
 from django.db import models
-from django.db import connection
 
-class ExpField(models.FloatField):
-
-    def __init__(self, *args, **kwargs):
-        # Have a default "default" set to 0.
-        if kwargs.get('default') is None:
-            kwargs['default'] = 0
-
-        super(ExpField, self).__init__(*args, **kwargs)
-
-
-class LevelField(models.IntegerField):
-
-    def __init__(self, *args, **kwargs):
-        # Have a default "default" set to 1.
-        if kwargs.get('default') is None:
-            kwargs['default'] = 1
-
-        self.exp_field = kwargs.pop('exp_field', None)
-
-        super(LevelField, self).__init__(*args, **kwargs)
 
 class Skills(models.Model):
+    overall_exp = models.FloatField(default=0)
+    overall = models.IntegerField(default=1)
 
-    overall_exp = ExpField()
-    overall = LevelField(exp_field=overall_exp)
+    attack_exp = models.FloatField(default=0)
+    attack = models.IntegerField(default=1)
 
-    attack_exp = ExpField()
-    attack = LevelField(exp_field=attack_exp)
+    defence_exp = models.FloatField(default=0)
+    defence = models.IntegerField(default=1)
 
-    defence_exp = ExpField()
-    defence = LevelField(exp_field=defence_exp)
+    strength_exp = models.FloatField(default=0)
+    strength = models.IntegerField(default=1)
 
-    strength_exp = ExpField()
-    strength = LevelField(exp_field=strength_exp)
+    constitution_exp = models.FloatField(default=0)
+    constitution = models.IntegerField(default=1)
 
-    constitution_exp = ExpField()
-    constitution = LevelField(exp_field=constitution_exp)
+    ranged_exp = models.FloatField(default=0)
+    ranged = models.IntegerField(default=1)
 
-    ranged_exp = ExpField()
-    ranged = LevelField(exp_field=ranged_exp)
+    prayer_exp = models.FloatField(default=0)
+    prayer = models.IntegerField(default=1)
 
-    prayer_exp = ExpField()
-    prayer = LevelField(exp_field=prayer_exp)
+    magic_exp = models.FloatField(default=0)
+    magic = models.IntegerField(default=1)
 
-    magic_exp = ExpField()
-    magic = LevelField(exp_field=magic_exp)
+    cooking_exp = models.FloatField(default=0)
+    cooking = models.IntegerField(default=1)
 
-    cooking_exp = ExpField()
-    cooking = LevelField(exp_field=cooking_exp)
+    woodcutting_exp = models.FloatField(default=0)
+    woodcutting = models.IntegerField(default=1)
 
-    woodcutting_exp = ExpField()
-    woodcutting = LevelField(exp_field=woodcutting_exp)
+    fletching_exp = models.FloatField(default=0)
+    fletching = models.IntegerField(default=1)
 
-    fletching_exp = ExpField()
-    fletching = LevelField(exp_field=fletching_exp)
+    fishing_exp = models.FloatField(default=0)
+    fishing = models.IntegerField(default=1)
 
-    fishing_exp = ExpField()
-    fishing = LevelField(exp_field=fishing_exp)
+    firemaking_exp = models.FloatField(default=0)
+    firemaking = models.IntegerField(default=1)
 
-    firemaking_exp = ExpField()
-    firemaking = LevelField(exp_field=firemaking_exp)
+    crafting_exp = models.FloatField(default=0)
+    crafting = models.IntegerField(default=1)
 
-    crafting_exp = ExpField()
-    crafting = LevelField(exp_field=crafting_exp)
+    smithing_exp = models.FloatField(default=0)
+    smithing = models.IntegerField(default=1)
 
-    smithing_exp = ExpField()
-    smithing = LevelField(exp_field=smithing_exp)
+    mining_exp = models.FloatField(default=0)
+    mining = models.IntegerField(default=1)
 
-    mining_exp = ExpField()
-    mining = LevelField(exp_field=mining_exp)
+    herblore_exp = models.FloatField(default=0)
+    herblore = models.IntegerField(default=1)
 
-    herblore_exp = ExpField()
-    herblore = LevelField(exp_field=herblore_exp)
+    agility_exp = models.FloatField(default=0)
+    agility = models.IntegerField(default=1)
 
-    agility_exp = ExpField()
-    agility = LevelField(exp_field=agility_exp)
+    thieving_exp = models.FloatField(default=0)
+    thieving = models.IntegerField(default=1)
 
-    thieving_exp = ExpField()
-    thieving = LevelField(exp_field=thieving_exp)
+    slayer_exp = models.FloatField(default=0)
+    slayer = models.IntegerField(default=1)
 
-    slayer_exp = ExpField()
-    slayer = LevelField(exp_field=slayer_exp)
+    farming_exp = models.FloatField(default=0)
+    farming = models.IntegerField(default=1)
 
-    farming_exp = ExpField()
-    farming = LevelField(exp_field=farming_exp)
+    runecrafting_exp = models.FloatField(default=0)
+    runecrafting = models.IntegerField(default=1)
 
-    runecrafting_exp = ExpField()
-    runecrafting = LevelField(exp_field=runecrafting_exp)
+    hunter_exp = models.FloatField(default=0)
+    hunter = models.IntegerField(default=1)
 
-    hunter_exp = ExpField()
-    hunter = LevelField(exp_field=hunter_exp)
+    construction_exp = models.FloatField(default=0)
+    construction = models.IntegerField(default=1)
 
-    construction_exp = ExpField()
-    construction = LevelField(exp_field=construction_exp)
+    summoning_exp = models.FloatField(default=0)
+    summoning = models.IntegerField(default=1)
 
-    summoning_exp = ExpField()
-    summoning = LevelField(exp_field=summoning_exp)
-
-    dungeoneering_exp = ExpField()
-    dungeoneering = LevelField(exp_field=dungeoneering_exp)
+    dungeoneering_exp = models.FloatField(default=0)
+    dungeoneering = models.IntegerField(default=1)
 
     user_name = models.CharField(max_length=30, primary_key=True)
 
     def get_skills(self):
-        # Returns a list of dictionnaries containing name, level and XP
-        # for each of the model's skills.
+        """
+        Get a player's stats
+        :return: a list of dictionaries containing name, rank, level and exp for each skill
+        """
         skill_values = []
 
-        # Iterate over the model's fields.
-        for field in self._meta.get_fields():
-            if isinstance(field, LevelField):
-                level = getattr(self, field.name)
-                exp = getattr(self, field.exp_field.name)
-                players = Skills.objects.order_by("-%s" % field.exp_field.name).all()
-                rank = [i+1 for i, player in enumerate(players) if player.user_name == self.user_name][0]
-                skill_values.append({
-                    'name': field.name.title(),
-                    'rank' : rank,
-                    'level': level,
-                    'exp': int(exp),
-                })
-
+        from utils import skill_names
+        for skill in skill_names:
+            level_name = self._meta.get_field(skill).name
+            exp_name = self._meta.get_field(skill + '_exp').name
+            level = getattr(self, level_name)
+            exp = getattr(self, exp_name)
+            players = Skills.objects.order_by("-%s" % exp_name).all()
+            rank = [i + 1 for i, player in enumerate(players) if player.user_name == self.user_name][0]
+            skill_values.append({
+                'name': skill.title(),
+                'rank': rank,
+                'level': level,
+                'exp': int(exp),
+            })
         return skill_values
 
     def __str__(self):
