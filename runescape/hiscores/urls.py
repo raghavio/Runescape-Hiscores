e19e9014472bfadfe088cccb8cec3da9fa9a1541
Raghav Sharma <raghav_ftw@hotmail.com>
2015-10-19 03:39:55 +0530

    Given names to urls for url reversing

--- runescape/hiscores/urls.py
+++ runescape/hiscores/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^compare/(?P<player1>[\@\.\w-]+)/(?P<player2>[\@\.\w-]+)/$', views.compare),
-    url(r'^player/(?P<user_name>.*)/$', views.player),
-    url(r'^(?P<skill>[a-zA-Z]+)/$', views.show_skill),
+    url(r'^compare/(?P<player1>[\@\.\w-]+)/(?P<player2>[\@\.\w-]+)/$', views.compare, name='compare'),
+    url(r'^player/(?P<user_name>.*)/$', views.player, name='player'),
+    url(r'^(?P<skill>[a-zA-Z]+)/$', views.show_skill, name='skill'),
 ]
